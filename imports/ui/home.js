fce3060b32150151669be6befa04351869579cf6
pgAdmin <punit.g7@gmail.com>
2016-09-01 12:33:45 +0530

    Adds the landing page

--- imports/ui/home.js
+++ imports/ui/home.js
@@ -9,144 +9,199 @@ Template.myHome.helpers({
 
 Template.myHome.onRendered(function bodyOnRendered() {
   $(function () {
-    $('.typer').typed({
-      strings: [ 'financial planner^3000', 'insurance broker^3000', 'lender^3000' ],
+    $(".typer").typed({
+      strings: ["financial planner.^3000", "insurance broker.^3000", "lender.^3000"],
       typeSpeed: 1,
       loop: true,
       loopCount: false
     });
   });
 
-  $(document).ready(function () {
-    BrowserDetect.init();
+  var transparent = true;
 
-    if (BrowserDetect.browser == 'Explorer' && BrowserDetect.version <= 9) {
-      $('body').html(better_browser);
+  var transparentDemo = true;
+  var fixedTop = false;
+
+  var navbar_initialized = false;
+
+  materialKit = {
+    misc:{
+      navbar_menu_visible: 0
+    },
+
+    checkScrollForTransparentNavbar: debounce(function() {
+      if($(document).scrollTop() > 260 ) {
+        if(transparent) {
+          transparent = false;
+          $('.navbar-color-on-scroll').removeClass('navbar-transparent');
+        }
+      } else {
+        if( !transparent ) {
+          transparent = true;
+          $('.navbar-color-on-scroll').addClass('navbar-transparent');
+        }
+      }
+    }, 17),
+
+    initSliders: function(){
+      // Sliders for demo purpose
+      $('#sliderRegular').noUiSlider({
+        start: 40,
+        connect: "lower",
+        range: {
+          min: 0,
+          max: 100
+        }
+      });
+
+      $('#sliderDouble').noUiSlider({
+        start: [20, 60] ,
+        connect: true,
+        range: {
+          min: 0,
+          max: 100
+        }
+      });
     }
+  }
+
+  var big_image;
+
+  materialKitDemo = {
+    checkScrollForParallax: debounce(function(){
+      if(isElementInViewport(big_image)){
+        var current_scroll = $(this).scrollTop();
+
+        oVal = ($(window).scrollTop() / 3);
+        big_image.css({
+          'transform':'translate3d(0,' + oVal +'px,0)',
+          '-webkit-transform':'translate3d(0,' + oVal +'px,0)',
+          '-ms-transform':'translate3d(0,' + oVal +'px,0)',
+          '-o-transform':'translate3d(0,' + oVal +'px,0)'
+        });
+      }
+    }, 4),
+
+    initContactUsMap: function(){
+      var myLatlng = new google.maps.LatLng(44.433530, 26.093928);
+      var mapOptions = {
+        zoom: 14,
+        center: myLatlng,
+        styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
+        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+      }
+      var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        title:"Hello World!"
+      });
+      marker.setMap(map);
+    },
+
+    initContactUs2Map: function(){
+      var lat = 44.433530;
+      var long = 26.093928;
+
+      var centerLong = long - 0.025;
+
+      var myLatlng = new google.maps.LatLng(lat,long);
+      var centerPosition = new google.maps.LatLng(lat, centerLong);
+
+      var mapOptions = {
+        zoom: 14,
+        center: centerPosition,
+        styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
+        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
+      }
+      var map = new google.maps.Map(document.getElementById("contactUs2Map"), mapOptions);
+
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        title:"Hello World!"
+      });
+      marker.setMap(map);
+    }
+
+  }
+  // Returns a function, that, as long as it continues to be invoked, will not
+  // be triggered. The function will be called after it stops being called for
+  // N milliseconds. If `immediate` is passed, trigger the function on the
+  // leading edge, instead of the trailing.
+
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      clearTimeout(timeout);
+      timeout = setTimeout(function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      }, wait);
+      if (immediate && !timeout) func.apply(context, args);
+    };
+  };
+
+
+  function isElementInViewport(elem) {
+    var $elem = $(elem);
+
+    // Get the scroll position of the page.
+    var scrollElem = ((navigator.userAgent.toLowerCase().indexOf('webkit') != -1) ? 'body' : 'html');
+    var viewportTop = $(scrollElem).scrollTop();
+    var viewportBottom = viewportTop + $(window).height();
+
+    // Get the position of the element on the page.
+    var elemTop = Math.round( $elem.offset().top );
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemTop < viewportBottom) && (elemBottom > viewportTop));
+  }
+
+  $(document).ready(function () {
+
+    // Init Material scripts for buttons ripples, inputs animations etc, more info on the next link https://github.com/FezVrasta/bootstrap-material-design#materialjs
+    $.material.init();
 
     window_width = $(window).width();
-    window_height = $(window).height();
 
-    burger_menu = $('.navbar').hasClass('navbar-burger') ? true : false;
+    //  Activate the Tooltips
+    $('[data-toggle="tooltip"], [rel="tooltip"]').tooltip();
 
-    if (!Modernizr.touch) {
-      $('body').addClass('no-touch');
-      no_touch_screen = true;
+    // Activate Datepicker
+    if($('.datepicker').length != 0){
+        $('.datepicker').datepicker({
+             weekStart:1
+        });
     }
 
-    // Init navigation toggle for small screens
-    if (window_width < 992 || burger_menu) {
-      gaia.initRightMenu();
-    }
+    //    Activate bootstrap-select
+    $(".select").dropdown({ "dropdownClass": "dropdown-menu", "optionClass": "" });
 
-    if ($('.content-with-opacity').length != 0) {
-      content_opacity = 1;
-    }
+    // Activate Popovers
+    $('[data-toggle="popover"]').popover();
 
-    $navbar = $('.navbar[color-on-scroll]');
-    scroll_distance = $navbar.attr('color-on-scroll') || 500;
-
-    $('.google-map').each(function() {
-      var lng = $(this).data('lng');
-      var lat = $(this).data('lat');
-
-      gaia.initGoogleMaps(this, lat, lng);
+    // Active Carousel
+    $('.carousel').carousel({
+      interval: 400000
     });
 
-  });
-
-  // activate collapse right menu when the windows is resized
-  $(window).resize(function() {
-    if ($(window).width() < 992) {
-      gaia.initRightMenu();
-      //gaia.checkResponsiveImage();
+    //Activate tags
+    if($(".tagsinput").length != 0){
+      $(".tagsinput").tagsInput();
     }
 
-    if ($(window).width() > 992 && !burger_menu) {
-      $('nav[role="navigation"]').removeClass('navbar-burger');
-      gaia.misc.navbar_menu_visible = 1;
-      navbar_initialized = false;
-    }
-  });
-
-  $(window).on('scroll', function() {
-    gaia.checkScrollForTransparentNavbar();
-    if (window_width > 992) {
-      gaia.checkScrollForParallax();
+    if($('.navbar-color-on-scroll').length != 0){
+      $(window).on('scroll', materialKit.checkScrollForTransparentNavbar)
     }
 
-    if (content_opacity == 1) {
-      gaia.checkScrollForContentTransitions();
-    }
-  });
-
-  $( document ).ready(function() {
-    var header = $('#filter-home');
-    var location = $('#location');
-    var loans1 = $('#loans-1');
-    var loans2 = $('#loans-2');
-    var loans3 = $('#loans-3');
-    var lastDiv;
-    var thisDiv;
-
-    function fadeRightLeft(divOut, divIn) {
-      TweenLite.to(divOut, 1, {opacity:0, x:-200});
-      setTimeout(function () {
-        divOut.css('display', 'none');
-      }, 1000);
-
-      setTimeout(function () {
-        divIn.css('display', 'block');
-        TweenLite.from(divIn, 1, {opacity:0, x:200});
-        TweenLite.to(divIn, 1, {opacity:1})
-      }, 1000);
-
-      lastDiv = divOut;
-      thisDiv = divIn;
+    if (window_width >= 768){
+      big_image = $('.page-header[data-parallax="active"]');
+      if(big_image.length != 0){
+        $(window).on('scroll', materialKitDemo.checkScrollForParallax);
+      }
     }
 
-    function fadeLeftRight(divOut, divIn) {
-      TweenLite.to(divOut, 1, {opacity:0, x:200});
-      setTimeout(function () {
-        divOut.css('display', 'none');
-      }, 1000);
-
-      setTimeout(function () {
-        divIn.css('display', 'block');
-        TweenLite.from(divIn, 1, {opacity:0, x:-200});
-        TweenLite.to(divIn, 1, {opacity:1})
-      }, 1000);
-
-      lastDiv = divOut;
-      thisDiv = divIn;
-    }
-
-
-    $('#back').click(function() {
-      fadeLeftRight(thisDiv, lastDiv);
-    });
-
-
-    $('#Loans').click(function() {
-      console.log($(this).attr('id'));
-      $('.homeParallax').find('.image').addClass('blur');
-      fadeRightLeft(header, loans1);
-    });
-
-    $('.loans1').click(function() {
-      console.log($(this).attr('id'));
-      fadeRightLeft(loans1, loans2);
-    });
-
-    $('.loans2').click(function() {
-      console.log($(this).attr('id'));
-      fadeRightLeft(loans2, loans3);
-    });
-
-    $('.loans3').click(function() {
-      console.log($(this).attr('id'));
-      fadeRightLeft(loans3, location);
-    });
   });
 
 });
