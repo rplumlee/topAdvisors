1a33df37f0c2f4e23080eb60da9fefe8e70c779f
pgAdmin <punit.g7@gmail.com>
2016-09-19 14:57:54 +0530

    Reorganized file structure and added admin leads page

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
-import '../imports/ui/home.js';
-import '../imports/ui/login.js';
-import '../imports/ui/dashboard.js';
+import '../imports/ui/home';
+import '../imports/ui/login';
+import '../imports/ui/dashboard';
+import '../imports/ui/admin/leads';
 
 Router.route('/', {
   onRun: function () {
@@ -29,5 +30,15 @@ Router.route('/dashboard', {
   },
   action: function () {
     this.render('myDashboard');
+  }
+});
+
+Router.route('/admin', {
+  onRun: function () {
+    window.scrollTo(0, 0);
+    this.next();
+  },
+  action: function () {
+    this.render('adminLeads');
   }
 });
---
+++ imports/ui/admin/leads/index.js
@@ -0,0 +1,52 @@
+import { Template } from 'meteor/templating';
+import Collections from '/lib/collections';
+import './manage_leads.html';
+
+Template.adminLeads.helpers({
+  copyrightDate: function () {
+    return new Date().getFullYear();
+  },
+  leads: function () {
+    return Collections.Leads.find();
+  },
+  views: function () {
+    return Collections.Activities.find({ type: 'viewProfile' });
+  },
+  parseDate: function (date) {
+    return date.getMonth() + '/' + date.getDate() + '/' + date.getFullYear();
+  },
+  statusLabel: function (status) {
+    return (status == 'open');
+  },
+  user: function () {
+    return Meteor.user()
+  }
+});
+
+Template.adminLeads.events({
+  'click #logout'(event) {
+    Meteor.logout(() => {
+      Router.go('login');
+    });
+  }
+});
+
+Template.myDashboard.onRendered(function bodyOnRendered() {
+  if (!Meteor.userId()) {
+    Router.go('login');
+  }
+
+  document.title = 'Leads Dashboard';
+  Meteor.subscribe('leads.list');
+  Meteor.subscribe('activities.list');
+
+  //
+  // paper-dashboard.js
+  //
+  window_width = $(window).width();
+
+  // Init navigation toggle for small screens
+  if (window_width <= 991) {
+    lbd.initRightMenu();
+  }
+});
