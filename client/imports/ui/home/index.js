22b2c7ab403c541fcd6a46d544c65b4d75c98582
pgAdmin <punit.g7@gmail.com>
2016-09-30 01:11:51 +0530

    Changes the home page

--- client/imports/ui/home/index.js
+++ client/imports/ui/home/index.js
@@ -8,6 +8,8 @@ Template.myHome.helpers({
 });
 
 Template.myHome.onRendered(function bodyOnRendered() {
+  new Vivus('my-svg', {duration: 200});
+
   $(function () {
     $(".typer").typed({
       strings: ["financial planner.^3000", "insurance broker.^3000", "lender.^3000"],
@@ -81,49 +83,8 @@ Template.myHome.onRendered(function bodyOnRendered() {
         });
       }
     }, 4),
-
-    initContactUsMap: function(){
-      var myLatlng = new google.maps.LatLng(44.433530, 26.093928);
-      var mapOptions = {
-        zoom: 14,
-        center: myLatlng,
-        styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
-        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-      }
-      var map = new google.maps.Map(document.getElementById("contactUsMap"), mapOptions);
-
-      var marker = new google.maps.Marker({
-        position: myLatlng,
-        title:"Hello World!"
-      });
-      marker.setMap(map);
-    },
-
-    initContactUs2Map: function(){
-      var lat = 44.433530;
-      var long = 26.093928;
-
-      var centerLong = long - 0.025;
-
-      var myLatlng = new google.maps.LatLng(lat,long);
-      var centerPosition = new google.maps.LatLng(lat, centerLong);
-
-      var mapOptions = {
-        zoom: 14,
-        center: centerPosition,
-        styles: [{"featureType":"water","stylers":[{"saturation":43},{"lightness":-11},{"hue":"#0088ff"}]},{"featureType":"road","elementType":"geometry.fill","stylers":[{"hue":"#ff0000"},{"saturation":-100},{"lightness":99}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"color":"#808080"},{"lightness":54}]},{"featureType":"landscape.man_made","elementType":"geometry.fill","stylers":[{"color":"#ece2d9"}]},{"featureType":"poi.park","elementType":"geometry.fill","stylers":[{"color":"#ccdca1"}]},{"featureType":"road","elementType":"labels.text.fill","stylers":[{"color":"#767676"}]},{"featureType":"road","elementType":"labels.text.stroke","stylers":[{"color":"#ffffff"}]},{"featureType":"poi","stylers":[{"visibility":"off"}]},{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#b8cb93"}]},{"featureType":"poi.park","stylers":[{"visibility":"on"}]},{"featureType":"poi.sports_complex","stylers":[{"visibility":"on"}]},{"featureType":"poi.medical","stylers":[{"visibility":"on"}]},{"featureType":"poi.business","stylers":[{"visibility":"simplified"}]}],
-        scrollwheel: false, //we disable de scroll over the map, it is a really annoing when you scroll through page
-      }
-      var map = new google.maps.Map(document.getElementById("contactUs2Map"), mapOptions);
-
-      var marker = new google.maps.Marker({
-        position: myLatlng,
-        title:"Hello World!"
-      });
-      marker.setMap(map);
-    }
-
   }
+
   // Returns a function, that, as long as it continues to be invoked, will not
   // be triggered. The function will be called after it stops being called for
   // N milliseconds. If `immediate` is passed, trigger the function on the
