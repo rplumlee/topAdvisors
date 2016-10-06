d7ede6550d3c12b7013c9417939f7ad27afe82ab
pgAdmin <punit.g7@gmail.com>
2016-10-06 16:39:24 +0530

    Fixes lint issues

--- server/users/methods.js
+++ server/users/methods.js
@@ -2,7 +2,7 @@
  * Define the exported methods for the accounts module
  * @param {object} context - server application context
  */
-export default function ({ Meteor, Accounts, Roles, Collections, check }) {
+export default function ({ Meteor, Accounts, Collections, check }) {
 
   Meteor.methods({
 
@@ -13,12 +13,6 @@ export default function ({ Meteor, Accounts, Roles, Collections, check }) {
      */
     'users.create': function (params) {
       check(params, Object);
-
-      // Ensure logged in user is an admin
-      // lib.authorize([ roles.admin ]);
-
-      // Check the roles and parent org selected for the new users
-      // _checkRoles(data.role);
 
       // Create user
       var id = Accounts.createUser(params);
