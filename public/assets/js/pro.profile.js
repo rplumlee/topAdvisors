e208d2a255995e106d48fe259d55e615f9dbade2
reidj <rplumlee@datadrivendesign.net>
2017-04-07 17:06:12 -0700

    profile page redesign - just desktop

--- public/assets/js/pro.profile.js
+++ public/assets/js/pro.profile.js
@@ -2,19 +2,7 @@
 (function ($) {
   $(document).ready(function() {
 
-    // fade in .navbar
-    $(function () {
-      $(window).scroll(function () {
-        // set distance user needs to scroll before we start fadeIn
-        if ($(this).scrollTop() > 570) {
-          $('.profile-drop').css('top', '0px');
-        } else {
-          $('.profile-drop').css('top', '-100px');
-        }
-      });
-    });
-
-    $('.contact-pro-form').submit(function(e){
+   $('.contact-pro-form').submit(function(e){
       e.preventDefault();
 
       var fields = [
@@ -74,36 +62,49 @@
 }(jQuery));
 
 window.onload = function () {
-  var chart = new CanvasJS.Chart("chartContainer", {
-    title: {
-      text: ""
-    },
-    animationEnabled: true,
-    legend: {
-      verticalAlign: "center",
-      horizontalAlign: "right"
-    },
-    data: [
-      {
-        indexLabelFontSize: 18,
-        indexLabelFontFamily: "Helvetica",
-        indexLabelFontColor: "#444444",
-        indexLabelLineColor: "#999999",
-        indexLabelPlacement: "inside",
-        type: "pie",
-        showInLegend: true,
-        toolTipContent: "<strong>{legendText} - #percent%</strong>",
-        dataPoints: window.specialties
-      }
-    ]
-  });
-  chart.render();
 
-  var data = window.sessionStorage.getItem('lead');
-  data = data ? JSON.parse(data) : {};
-  for (props in data) {
-    if (document.getElementById(props)) {
-      document.getElementById(props).value = data[props];
+
+    var ctx = document.getElementById("chartContainer");
+    var labels = [];
+    var amount = [];
+    for (i = 0; i < window.specialties.length; i++){
+        labels.push(window.specialties[i].legendText);
+        amount.push(window.specialties[i].y);
     }
-  }
+
+    var data = {
+        labels: labels,
+        datasets: [
+            {
+                data: amount,
+                backgroundColor: [
+                    "#36A2EB",
+                    "#FF6384",
+                    "#FFCE56",
+                    "#00BCD4",
+                    "#9C27B0"
+                ],
+                hoverBackgroundColor: [
+                    "#36A2EB",
+                    "#FF6384",
+                    "#FFCE56",
+                    "#00BCD4",
+                    "#9C27B0"
+                ]
+            }]
+    };
+
+    var myPieChart = new Chart(ctx,{
+        type: 'doughnut',
+        data: data
+    });
+
+
+    var data = window.sessionStorage.getItem('lead');
+    data = data ? JSON.parse(data) : {};
+    for (props in data) {
+        if (document.getElementById(props)) {
+            document.getElementById(props).value = data[props];
+        }
+    }
 }
